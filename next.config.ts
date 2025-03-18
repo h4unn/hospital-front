c76a04031144591fd456b16aecd744c6d44a7f80
hyunnam-cho <wiwi154@naver.com>
2025-03-18 13:54:22 +0900

    [조현남]cors 프록시 서버 우회

--- next.config.ts
+++ next.config.ts
@@ -7,6 +7,10 @@ const nextConfig: NextConfig = {
         source: "/api/:path*",
         destination: "http://localhost:4000/api/:path*",
       },
+      {
+        source: "/api/:path*",
+        destination: "http://52.79.233.55:4000/api/:path*",
+      },
     ];
   },
   env: {
