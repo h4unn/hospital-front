d4196cc2c5daa6c578bbd508794fb5f955788f43
hyunnam-cho <wiwi154@naver.com>
2025-03-24 16:23:08 +0900

    update test2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@
 import ReservationView from "@/views/reservation/ReservationView";
 
 const Home = () => {
-  console.log("update4");
+  console.log("update5");
   return (
     <>
       <ReservationView />
